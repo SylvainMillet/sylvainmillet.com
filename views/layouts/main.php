0c3f0b5419395f80a489fc6b8d0c48df58f06ad0
Sylvain Millet <kira69007@gmail.com>
2021-01-19 20:25:48 +0100

    finalisation YII

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -50,6 +50,9 @@ AppAsset::register($this);
 
     <div class="container">
         <?= Breadcrumbs::widget([
+            'homeLink' => [
+                'label' => 'Accueil', 'url' => ['/site/index']
+            ],
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
         <?= Alert::widget() ?>
